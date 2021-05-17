faf3b68e7225f69bd07215f45ae926d191676a91
yujianbo <yujianbo@xiaomi.com>
2021-05-17 15:04:54 +0800

    add

---
+++ fasthttp/sentryfasthttp.go
@@ -0,0 +1,137 @@
+package sentryfasthttp
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/getsentry/sentry-go"
+	"github.com/valyala/fasthttp"
+)
+
+type contextKey int
+
+const ContextKey = contextKey(1)
+const valuesKey = "sentry"
+
+type Handler struct {
+	repanic         bool
+	waitForDelivery bool
+	timeout         time.Duration
+}
+
+type Options struct {
+	// Repanic configures whether Sentry should repanic after recovery, in most cases it should be set to false,
+	// as fasthttp doesn't include it's own Recovery handler.
+	Repanic bool
+	// WaitForDelivery configures whether you want to block the request before moving forward with the response.
+	// Because fasthttp doesn't include it's own Recovery handler, it will restart the application,
+	// and event won't be delivered otherwise.
+	WaitForDelivery bool
+	// Timeout for the event delivery requests.
+	Timeout time.Duration
+}
+
+// New returns a struct that provides Handle method
+// that satisfy fasthttp.RequestHandler interface.
+func New(options Options) *Handler {
+	handler := Handler{
+		repanic:         false,
+		timeout:         time.Second * 2,
+		waitForDelivery: false,
+	}
+
+	if options.Repanic {
+		handler.repanic = true
+	}
+
+	if options.Timeout != 0 {
+		handler.timeout = options.Timeout
+	}
+
+	if options.WaitForDelivery {
+		handler.waitForDelivery = true
+	}
+
+	return &handler
+}
+
+// Handle wraps fasthttp.RequestHandler and recovers from caught panics.
+func (h *Handler) Handle(handler fasthttp.RequestHandler) fasthttp.RequestHandler {
+	return func(ctx *fasthttp.RequestCtx) {
+		hub := sentry.CurrentHub().Clone()
+		scope := hub.Scope()
+		scope.SetRequest(convert(ctx))
+		scope.SetRequestBody(ctx.Request.Body())
+		ctx.SetUserValue(valuesKey, hub)
+		defer h.recoverWithSentry(hub, ctx)
+		handler(ctx)
+	}
+}
+
+func (h *Handler) recoverWithSentry(hub *sentry.Hub, ctx *fasthttp.RequestCtx) {
+	if err := recover(); err != nil {
+		eventID := hub.RecoverWithContext(
+			context.WithValue(context.Background(), sentry.RequestContextKey, ctx),
+			err,
+		)
+		if eventID != nil && h.waitForDelivery {
+			hub.Flush(h.timeout)
+		}
+		if h.repanic {
+			panic(err)
+		}
+	}
+}
+
+// GetHubFromContext retrieves attached *sentry.Hub instance from fasthttp.RequestCtx.
+func GetHubFromContext(ctx *fasthttp.RequestCtx) *sentry.Hub {
+	hub := ctx.UserValue(valuesKey)
+	if hub, ok := hub.(*sentry.Hub); ok {
+		return hub
+	}
+	return nil
+}
+
+func convert(ctx *fasthttp.RequestCtx) *http.Request {
+	defer func() {
+		if err := recover(); err != nil {
+			sentry.Logger.Printf("%v", err)
+		}
+	}()
+
+	r := new(http.Request)
+
+	r.Method = string(ctx.Method())
+	uri := ctx.URI()
+	// Ignore error.
+	r.URL, _ = url.Parse(fmt.Sprintf("%s://%s%s", uri.Scheme(), uri.Host(), uri.Path()))
+
+	// Headers
+	r.Header = make(http.Header)
+	r.Header.Add("Host", string(ctx.Host()))
+	ctx.Request.Header.VisitAll(func(key, value []byte) {
+		r.Header.Add(string(key), string(value))
+	})
+	r.Host = string(ctx.Host())
+
+	// Cookies
+	ctx.Request.Header.VisitAllCookie(func(key, value []byte) {
+		r.AddCookie(&http.Cookie{Name: string(key), Value: string(value)})
+	})
+
+	// Env
+	r.RemoteAddr = ctx.RemoteAddr().String()
+
+	// QueryString
+	r.URL.RawQuery = string(ctx.URI().QueryString())
+
+	// Body
+	r.Body = ioutil.NopCloser(bytes.NewReader(ctx.Request.Body()))
+
+	return r
+}
